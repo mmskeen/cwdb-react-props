723d3afb5f074d3d3a390b44dc877014b1865c42
Michael Skeen <mmskeen@gmail.com>
2020-03-25 16:12:22 -0600

    Did exercises with props

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Card(props) {
+  console.log(props);
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.phone}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+export default Card;
